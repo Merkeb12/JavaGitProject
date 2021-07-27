41e1704e49f8ec6aeff7bbbc719e31d36596fd0d
Merkeb <merkeb12@yahoo.com>
2021-07-27 10:33:42 -0400

    Initial commit system out println statements

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Merkeb Mekuria ye Kao Zemba lij");
+        System.out.println("Ochollo the land of brands");
+        System.out.println("Deacon curator manager president researcher doctor Merkeb");
+
+    }
+}
